0063944c376d2440edaade62b0596e8e2d5cd926
Sagar Tamang <sagar.bdr0000@gmail.com>
2026-06-05 16:16:27 +0530

    chore/fix: add fixed grid @ writings

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -30,8 +30,8 @@ export function BlogPosts({ limit }: BlogPostsProps) {
           className="flex flex-col gap-figma-inside-gap group"
           href={`/blog/${post.slug}`}
         >
-          <div className="w-full flex flex-row md:flex-row items-center space-x-0 md:space-x-2">
-            <p className="text-neutral-600 dark:text-neutral-400 mr-4 md:mr-10 tabular-nums shrink-0">
+          <div className="w-full grid grid-cols-[8rem_1fr] md:grid-cols-[9rem_1fr] items-baseline gap-2">
+            <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
               {formatDate(post.metadata.publishedAt, false)}
             </p>
             <p className="text-neutral-900 dark:text-neutral-100 tracking-tight underline hover:decoration-neutral-400 underline-offset-4">
