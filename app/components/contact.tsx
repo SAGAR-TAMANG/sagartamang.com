c17f9de42b95f3be426a44b71ffd655193d83c10
Sagar Tamang <sagar.bdr0000@gmail.com>
2026-06-11 16:57:00 +0530

    chore: contact twospoon link attractive

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { HoverLinkPreview } from "./hover-link-preview"
 
 const Contact = () => {
@@ -17,15 +18,23 @@ const Contact = () => {
           </a>.
         </p>
         <p>
-          for enterprise ai solutions, reach out to {' '}
-          <HoverLinkPreview
-            href="https://twospoon.ai/"
-            previewImage="/previews/twospoon.webp"
-            imageAlt="TwoSpoon.ai preview"
-            className="underline hover:decoration-muted-foreground underline-offset-4"
-          >
-            twospoon.ai
-          </HoverLinkPreview>.
+          for enterprise ai solutions, reach out at {' '}
+          <span className="inline-flex align-middle gap-0.5 bg-white text-black rounded-sm px-0.5 py-0 hover:cursor-pointer hover:bg-white/90 active:bg-white/70 transition leading-4">
+            <Image
+              className="invert"
+              src={"/twospoon-logo-favicon.svg"}
+              alt="favicon"
+              height={12}
+              width={12}
+            />
+            <HoverLinkPreview
+              href="https://twospoon.ai/"
+              previewImage="/previews/twospoon.webp"
+              imageAlt="TwoSpoon.ai preview"
+            >
+              twospoon.ai
+            </HoverLinkPreview>
+          </span>.
         </p>
         <p>
           or just read my {' '}
